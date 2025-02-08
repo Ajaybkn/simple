5cb380495bd2c5e629fbd73cacfd7f53c33273c0
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-02-08 13:49:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World from Node.js in Docker!");
+});
+// app.get("/fuck", (req, res) => {
+// 	res.send("Fuck the  World from Node.js in Docker!");
+// });
+
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
