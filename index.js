edd8a353c706ce28048b1054556cf2b87838f697
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-02-08 15:45:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello World from Node.js in Docker!");
+	res.send("Hello World from Node.js in Docker! in private repo");
 });
 
 
