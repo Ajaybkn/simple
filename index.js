5d087436202182d142108d329aea239a21ee0be1
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-02-11 12:44:34 +0530

    new

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 console.log("hello");
+console.log("new");
 app.get("/", (req, res) => {
 	res.send("Hello heyy World from Node.js in Docker hello! ");
 });
